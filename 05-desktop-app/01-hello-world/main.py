14ac582bc5171a008739a8125e7336ab99993ead
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-12-02 17:34:59 +0000

    add 05-desktop-app

---
+++ 05-desktop-app/01-hello-world/main.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class MainApp(App):
+
+    def build(self):
+        return Button(text="Hello World")
+
+
+if __name__ == '__main__':
+    app = MainApp(title="Hola, mundo Kivy")
+    app.run()
