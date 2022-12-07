2673303876211692b33fe9b2e58882bfa3abd0c9
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-12-07 17:49:37 +0000

    add seaborn excercise

--- 06-data-science/seaborn-02/seaborn-02.py
+++ 06-data-science/seaborn-02/seaborn-02.py
@@ -20,55 +20,55 @@ def save_plot(name):
 
 def example1():
     sns.catplot(x="survived", kind="count", palette="ch:.25", data=titanic)
-    save_plot('example1.png')
+    save_plot('example01.png')
 
 
 def example2():
     sns.catplot(x="pclass", hue="survived", kind="count", palette="pastel",
                 edgecolor=".6", data=titanic)
-    save_plot('example2.png')
+    save_plot('example02.png')
 
 
 def example3():
     sns.catplot(x="sex", hue="survived", kind="count", palette="pastel",
                 edgecolor=".6", data=titanic)
-    save_plot('example3.png')
+    save_plot('example03.png')
 
 
 def example4():
     sns.catplot(x="embarked", hue="survived", kind="count", palette="pastel",
                 edgecolor=".6", data=titanic)
-    save_plot('example4.png')
+    save_plot('example04.png')
 
 
 def example5():
     sns.catplot(x="sibsp", hue="survived", kind="count", palette="pastel",
                 edgecolor=".6", data=titanic)
-    save_plot('example5.png')
+    save_plot('example05.png')
 
 
 def example6():
     titanic['age'] = pd.cut(titanic['age'], [0, 10, 20, 30, 40, 50, 80])
     sns.catplot(x="age", hue="survived", kind="count", palette="pastel",
                 edgecolor=".6", data=titanic)
-    save_plot('example6.png')
+    save_plot('example06.png')
 
 
 def example7():
     titanic['fare'] = pd.cut(titanic['fare'], [0, 10, 20, 30, 40, 50, 80])
     g = sns.FacetGrid(titanic, col="survived", row="fare")
     g.map(plt.hist, "age")
-    save_plot('example7.png')
+    save_plot('example07.png')
 
 
 def example8():
     sns.catplot(x="pclass", y="age", hue="sex", kind="swarm", data=titanic)
-    save_plot('example8.png')
+    save_plot('example08.png')
 
 
 def example9():
     sns.jointplot(x="fare", y="age", data=titanic)
-    save_plot('example9.png')
+    save_plot('example09.png')
 
 
 def example10():
@@ -97,4 +97,4 @@ def example14():
 
 
 if __name__ == '__main__':
-    example14()
+    example9()
